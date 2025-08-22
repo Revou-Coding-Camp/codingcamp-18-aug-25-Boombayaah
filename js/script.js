2b02b0a40cca3ce23df9ce7a3c5be42c4f00816b
Boombayaah <fernandoliepino@gmail.com>
2025-08-22 23:16:06 +0700

    Perbaikan HTML dan JS

--- js/script.js
+++ js/script.js
@@ -1,17 +1,40 @@
-let list_to_do = [];
+let listToDo = [];
 
 // Validate Form Inputs
 function ValidateForm() {
-    // DOM Form Elements
-    const task_input = document.getElementById("task-input")
-    const due_date_input = document.getElementById("task-input")
+  // DOM Form Elements
+  const taskInput = document.getElementById("task-input");
+  const dueDateInput = document.getElementById("due-date-input");
 
-    if task_input.value != '' || due_date_input.value != '' {
-        AddToDo(task_input.value, due_date_input.value)
-    }
+  if (taskInput.value !== "" && dueDateInput.value !== "") {
+    AddToDo(taskInput.value, dueDateInput.value);
+  } else {
+    alert("Please enter a task!");
+  }
 }
 
 // Add A New To-Do
-function AddToDo() {
-    
+function AddToDo(taskInput, dueDateInput) {
+  listToDo.push({
+    task: taskInput,
+    dueDate: dueDateInput,
+  });
+
+  RenderToDoList();
+}
+
+// Render To-Do List
+function RenderToDoList() {
+  const taskList = document.getElementById("task-list");
+  taskList.innerHTML = "";
+
+  for (let i = 0; i < listToDo.length; i++) {
+    taskList.innerHTML += `<li class="border-b py-2">${listToDo[i].task} - Due: ${listToDo[i].dueDate}</li>`;
+  }
+}
+
+// Delete All To-Do
+function DeleteAll() {
+  listToDo = [];
+  RenderToDoList();
 }
